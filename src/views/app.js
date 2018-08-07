528f5a40170b628290c93fedb1fc28cf05496c10
Jeff Chasin <jeff@jeffchasin.com>
2018-08-06 23:18:23 -0400

    implement data passing to model & collection

--- src/views/app.js
+++ src/views/app.js
@@ -3,11 +3,14 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
   },
 
 
   render: function() {
+    // this.listView = new VideoListView(this.videos);
+    // this.$el.html(this.template(this.listView));
     this.$el.html(this.template());
     return this;
   },
