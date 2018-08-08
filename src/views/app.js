c2d7426bac208d8af525d86e2a459ec1ed0bd9c7
Jeff Chasin <jeff@jeffchasin.com>
2018-08-08 19:40:55 -0400

    render UI, add searchView, videoPlayerView

--- src/views/app.js
+++ src/views/app.js
@@ -12,6 +12,10 @@ var AppView = Backbone.View.extend({
 
     this.$el.html(this.template());
 
+    new SearchView({ el: '.search', model: this.videos }).render();
+
+    new VideoPlayerView({ el: '.player', model: this.videos }).render();
+
     new VideoListView({ el: '.list', collection: this.videos }).render();
 
     return this;
